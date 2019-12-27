85a341d85f9c3ddc6693bb20e82e7867bd446f33
2455191071 <2455191071@qq.com>
2019-12-27 09:30:36 +0800

    2019-12-27

--- src/main/java/com/bdqn/WorksystemApplication.java
+++ src/main/java/com/bdqn/WorksystemApplication.java
@@ -2,10 +2,12 @@ package com.bdqn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class WorksystemApplication {
 
+public class WorksystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorksystemApplication.class, args);
     }
